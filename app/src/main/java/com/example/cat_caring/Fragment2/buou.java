fcf3766c8af2c66f2e00ab3fdfe9f52f79b633af
Krain <1073115183@qq.com>
2020-12-15 11:25:54 +0800

    ljw supportv4

---
+++ app/src/main/java/com/example/cat_caring/Fragment2/buou.java
@@ -0,0 +1,15 @@
+package com.example.cat_caring.Fragment2;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cat_caring.R;
+
+public class buou extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.buou);
+    }
+}
---
+++ app/src/main/java/com/example/cat_caring/Fragment2/yingduan.java
@@ -0,0 +1,16 @@
+package com.example.cat_caring.Fragment2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cat_caring.R;
+
+public class yingduan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.yingduan);
+    }
+}
